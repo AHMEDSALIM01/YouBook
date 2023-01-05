0f5e9bfb9a214ab838e3d28437f34961c3510a0e
Ahmed Salim <93927978+AHMEDSALIM01@users.noreply.github.com>
2023-01-05 15:21:04 +0100

    génération de projet YoubookfrontEnd

---
+++ Backend/YouBook/src/test/java/com/youbook/YouBook/YouBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youbook.YouBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
